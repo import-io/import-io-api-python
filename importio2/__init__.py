552331dd117f28851d65dad4b0d428e1ec77017e
David Gwartney <david.gwartney@import.io>
2017-07-23 10:14:35 -0700

    Add missing imports and doc headers

--- importio2/__init__.py
+++ importio2/__init__.py
@@ -1,5 +1,5 @@
 #
-# Copyright 2016 Import.io
+# Copyright 2017 Import.io
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
